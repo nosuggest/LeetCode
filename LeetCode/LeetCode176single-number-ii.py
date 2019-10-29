9857e925bc7aada219779d0537b80d27f4dac34e
SladeSal <stw386@sina.com>
2019-10-29 09:35:41 +0800

    readme整理

--- LeetCode/LeetCode176single-number-ii.py
+++ LeetCode/LeetCode176single-number-ii.py
@@ -1,10 +1,18 @@
 # coding=utf-8
+
+'''
+更多解释在java代码注释块中
+  a b
+0 0 0
+1 x 0
+2 0 x
+3 0 0
+'''
 class Solution(object):
     def singleNumber(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-
 
         return (3*sum(set(nums))-sum(nums))/2
